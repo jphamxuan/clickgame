6209b46405bb7c15058f0673518364d0428389f2
Josiah Pham <josiah.pham@gmail.com>
2018-06-04 21:28:23 -0700

    1st commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
+import "./App.css";
+var shuffle = require('shuffle-array'),
+    collection = [1,2,3,4,5];
+ 
+shuffle(collection);
+ 
+console.log(collection);
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends
+  };
+
+
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
